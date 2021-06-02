2724026a90d2db7cb613e538165b905a97bacfad
José Alcántara <josearuk@gmail.com>
2021-06-02 22:14:08 +0200

    Added timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    const float MAX_TIME = 120;
+    public float timeRemaining;
+    [SerializeField] Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = MAX_TIME;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);  
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
+}
